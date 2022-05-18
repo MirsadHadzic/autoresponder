04eb8509ab2a09e7baa43c48d8e56cc962331c17
MirsadHadzic <mirsad.hadzic@stu.ibu.edu.ba>
2022-05-18 22:54:07 +0200

    f

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require 'vendor/autoload.php';
 Flight::route ('/', function(){ //define route and define function to handle request
-  echo 'Hello World!';
+  echo 'Hello Worldd!';
 });
 
 Flight::start();
